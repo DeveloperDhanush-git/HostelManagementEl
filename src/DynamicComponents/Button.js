2d4703c0b151ee4f2e406915f9dcb3a51924e18a
KarventhanFD <166226716+KarventhanFD@users.noreply.github.com>
2025-02-24 12:21:55 +0530

    Update Button.js

--- src/DynamicComponents/Button.js
+++ src/DynamicComponents/Button.js
@@ -1,1 +1,14 @@
 
+import React from 'react'
+
+const Button = ({button}) => {
+  return (
+    <div>
+        <button className="bg-[#69205D] text-white py-2 mt-3 rounded-md w-full text-sm md:text-base">
+        {button}
+      </button>
+    </div>
+  )
+}
+
+export default Button
