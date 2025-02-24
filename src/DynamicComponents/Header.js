3ba7a09e07a566c71ec35f0420e5376c621550ee
KarventhanFD <166226716+KarventhanFD@users.noreply.github.com>
2025-02-24 11:38:26 +0530

    Add files via upload

---
+++ src/DynamicComponents/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FaArrowLeft } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <div className="p-4 max-w-4xl mx-auto bg-white rounded-lg mt-1" style={{ fontFamily: 'Montserrat' }}>
+      <div className="flex items-center space-x-2 py-4">
+       
+        <FaArrowLeft className="text-xl cursor-pointer text-black hover:text-gray-700 transition duration-200" />
+
+        <div className="flex-grow bg-[#69205D] text-white p-4 flex justify-between items-center rounded-lg h-16">
+          <span className="text-2xl font-semibold">Maha Hostel</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
