ef1bbb020a9a28da621a7cd4c771ae099aaf9d47
riyaz1423 <riyazzmr19@gmail.com>
2025-02-25 12:09:57 +0530

    updated

--- mahahostel/src/TenentManagement/PropertyCard.js
+++ mahahostel/src/TenentManagement/PropertyCard.js
@@ -1,17 +1,22 @@
 const PropertyCard = () => {
-
   return (
-    <div className="flex justify-center items-center h-full"> 
-      <div className="border-2 border-[#69205D] rounded-md p-2 flex items-center gap-4 shadow-md bg-white max-w-md flex-shrink-0 mt-4">
-        {/* Left Section - Hostel Name & Logo */}
-        <div className="flex flex-col items-center w-20">
-          <h2 className="font-bold whitespace-nowrap mb-5 ml-11">Maha Hostel</h2>
-          <img src="asst/building icon.png" alt="Hostel Icon" className="ml-4" />
+    <div className="flex justify-center items-center">
+      <div className="border-2 border-[#69205D] rounded-lg p-3 flex items-center gap-4 bg-white max-w-2xl">
+        
+        {/* Left Section - Hostel Name & Icon */}
+        <div className="flex flex-col items-center w-32 mb-5">
+          <h2 className="font-bold text-lg self-start mt-2 mb-1 ">Maha Hostel</h2>
+          <div><img src="asst/building icon.png" alt="Hostel Icon" className="w-18 h-16 mr-6 mt-2" /></div>
         </div>
-
-           {/* Right Section - Address */}
-           <div className="bg-[#F7F7F7] p-2 rounded-md text-sm text-gray-600 w-60 mt-5 ml-9">
-        No. 03, ABC Road, ABC Street, ABC Colony, ABC City, ABC State,\n6xxxx6
+        
+        
+        {/* Right Section - Address */}
+        <div className="bg-gray-100 p-3 rounded-md text-sm text-gray-700 w-64 mt-2 ">
+          No. 03, ABC Road <br />
+          ABC Street, <br />
+          ABC Colony, <br />
+          ABC City, ABC State, <br />
+          6xxxx6
         </div>
       </div>
     </div>
