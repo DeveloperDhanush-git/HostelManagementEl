1c2fc9b92cb6ca691a11fce5afabbeb0e3e0fa63
Sneha Rajan <155979864+Sneha603@users.noreply.github.com>
2025-02-26 19:42:15 +0530

    Updated App.js

--- mahahostel/src/App.js
+++ mahahostel/src/App.js
@@ -5,12 +5,14 @@ import MyComponent from "./Components/MyComponent"; // Your property detail page
 
 const App = () => {
   return (
+    <div className="space-y-4 max-w-4xl mx-auto p-6 rounded-lg mt-1" style={{ fontFamily: "Montserrat" }}>
     <Router>
       <Routes>
         <Route path="/" element={<PropertyCard />} />
         <Route path="/property/:id" element={<MyComponent />} /> {/* Dynamic Route */}
       </Routes>
     </Router>
+    </div>
   );
 };
 
