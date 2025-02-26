e45b08ae6df90f8ebe77eeb5395d1e26c8e48ca3
KarventhanFD <166226716+KarventhanFD@users.noreply.github.com>
2025-02-26 10:05:24 +0530

    Update Header.js

--- src/DynamicComponents/Header.js
+++ src/DynamicComponents/Header.js
@@ -1,7 +1,6 @@
-
 import React from "react";
 import { FaArrowLeft, FaBed, FaPencilAlt, FaSlidersH, FaBell, FaSearch, FaPlus, FaCog } from "react-icons/fa";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const iconsMap = {
   arrowLeft: FaArrowLeft,
@@ -14,22 +13,34 @@ const iconsMap = {
   cog: FaCog
 };
 
-const Header = ({ title, icons }) => {
+const Header = ({ title, icons, onIconClick }) => {
   const navigate = useNavigate();
+  
   return (
     <div className="flex items-center space-x-2 py-4">
-         <FaArrowLeft
-        className="text-xl cursor-pointer text-black hover:text-gray-700 transition duration-200"
-        onClick={() => navigate(-1)}/>
+      <button
+        onClick={() => navigate(-1)}
+        className="bg-transparent text-black rounded-lg cursor-pointer"
+      >
+        <FaArrowLeft className="text-xl" />
+      </button>
 
       <div className="flex-grow bg-[#69205D] text-white p-4 flex justify-between items-center rounded-lg h-16">
-        <span className="text-xl font-semibold" >{title}</span>
+        <span className="text-xl font-semibold">{title}</span>
 
-        <div className="flex space-x-3 gap-1">
+        <div className="flex space-x-3">
           {icons &&
             icons.map((iconKey, index) => {
-              const IconComponent = iconsMap[iconKey];  
-              return IconComponent ? <IconComponent key={index} className="text-xl cursor-pointer" /> : null;
+              const IconComponent = iconsMap[iconKey];
+              return IconComponent ? (
+                <button
+                  key={index}
+                  className="bg-transparent text-white p-2 rounded-lg cursor-pointer"
+                  onClick={() => onIconClick && onIconClick(iconKey)} // Call the function when clicked
+                >
+                  <IconComponent className="text-xl" />
+                </button>
+              ) : null;
             })}
         </div>
       </div>
