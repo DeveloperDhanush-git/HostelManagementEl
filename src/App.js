1dcc92c4ca5d0788f92028491179dd9f6e6e7955
Dharani Malairaj <104492210+Dharani260901@users.noreply.github.com>
2025-02-27 18:32:40 +0530

    bed-inventory

--- src/App.js
+++ src/App.js
@@ -4,34 +4,30 @@ import BedInventory_1 from './Pages/BedInventory_1';
 import BedInventory_2 from './Pages/BedInventory_2';
 import BedInventory_3 from './Pages/BedInventory_3';
 import BedInventory_4 from './Pages/BedInventory_4';
-import BedInventory_5 from './Pages/BedInventory_5';
-import BedInventory_6 from './Pages/BedInventory_6';
+import { FormDataProvider } from './Components/PropertyContext';
+
+
 
 
 
 function App() {
   return (
-    <div>
-    <Routes>
+    <FormDataProvider>
+  
+     <Routes>
       <Route path='/' element={<BedInventory_1 />}/>
       <Route path='/step1' element={<BedInventory_2 />}/>
       <Route path='/step2' element={<BedInventory_3 /> }/>
       <Route path='/step3' element={<BedInventory_4 />}/>
-      <Route path='/step4' element={<BedInventory_5 />}/>
-      <Route path='/step5' element={<BedInventory_6 />}/>
+      
+    
     </Routes>
 
-     {/* <>
-    <BedInventory_1 />
-    <BedInventory_2 />
-    <BedInventory_3 />
-    <BedInventory_4 />
-    <BedInventory_5 />
-    <BedInventory_6 />
-    </>  */}
+     
       
      
-    </div>
+    
+    </FormDataProvider>
   );
 }
 
