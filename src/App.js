dc20048f47c9f2069486d9c24452ede9035cef3b
Sneha Rajan <155979864+Sneha603@users.noreply.github.com>
2025-02-27 13:40:55 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import HostelDetails from './Components/HostelDetails/HostelDetails';
 import EditPropertyDetails from './Components/EditPropertyDetails';
 import PropertyCard from './Components/PropertyCard/PropertyCard';
 import { SelectedRoomProvider } from './Components/SelectedRoomContext';
+import PropertyyCard from "./Components/PropertyyCard";
+import MyComponent from "./Components/MyComponent"; // Your property detail page
+import { useNavigate } from "react-router-dom";
+
+
 
 function App() {
   return (
+    
     <SelectedRoomProvider>
+      {/* <div className="space-y-4 max-w-4xl mx-auto p-6 rounded-lg mt-1" style={{ fontFamily: "Montserrat" }}></div> */}
+      {/* <div className=" bg-white min-h-screen rounded-lg  flex flex-col"></div> */}
       <Router>
         <Routes>
-          <Route path="/" element={<HostelDetails />} />
-          <Route path="/property-card" element={<PropertyCard />} />
+        <Route path="/" element={<PropertyyCard />} />
+
+        <Route path="/property/:id" element={<MyComponent />} /> {/* Dynamic Route */}
+        <Route path="/hostel-details" element={<HostelDetails />} />          <Route path="/property-card" element={<PropertyCard />} />
           <Route path="/edit-property" element={<EditPropertyDetails />} />
         </Routes>
       </Router>
