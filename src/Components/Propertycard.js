fb16845fc8be0035a024bcb7807389d755ee6a97
POOMANI M <poomani.m007@gmail.com>
2025-02-27 13:46:50 +0530

    Propertycard updated

--- src/Components/Propertycard.js
+++ src/Components/Propertycard.js
@@ -8,21 +8,22 @@ const PropertyCard = () => {
   console.log("🔹 Rendering PropertyCard with formData:", formData);
 
   return (
-    <div className="flex justify-center items-center p-4">
-      <div className="border-2 border-[#69205D] rounded-lg p-6 flex flex-col sm:flex-row items-center gap-8 shadow-md bg-white max-w-md w-full sm:max-w-md lg:max-w-lg">
+    <div className="flex justify-center items-center p-4  ">
+      <div className="border-2 border-[#69205D] rounded-lg p-6 flex flex-col sm:flex-row items-center gap-8 shadow-md bg-white max-w-4xl w-full">
+
         
        
-        <div className="flex flex-col items-center gap-5 w-full sm:w-auto">
-          <h2 className="font-bold text-md text-center">{formData?.name || "EL Codamics"}</h2>
+        <div className="flex flex-col items-center gap-3 w-full  sm:w-auto">
+          <h2 className="font-bold text-md text ml-18">{formData?.name || "EL Codamics"}</h2>
           <img 
             src={formData?.image || BuildingIcon} 
             alt="Property Icon" 
-            className="w-20 h-20"
+            className="w-24 h-24 ml-14"
           />
         </div>
 
         {/* Right Section - Address */}
-        <div className="bg-[#F7F7F7] p-3 rounded-md text-sm text-gray-600 w-full sm:w-2/3">
+        <div className="ml-8 bg-[#F7F7F7] p-3 rounded-md text-sm text-gray-600 w-full sm:w-2/3">
           <p>{formData?.houseNumber || "N/A"}</p>
           <p>{formData?.locality || "N/A"}</p>
           <p>{formData?.city || "N/A"}</p>
