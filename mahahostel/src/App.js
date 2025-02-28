452dafe85ae9dbb8ecb908d7bd744ec76fbbbc28
riyaz1423 <riyazzmr19@gmail.com>
2025-02-28 11:14:56 +0530

    update

--- mahahostel/src/App.js
+++ mahahostel/src/App.js
@@ -3,24 +3,19 @@ import ManageStaff from "./Component/ManageStaff";
 import AddStaff from "./Component/AddStaff";
 import SelectRole from "./Component/SelectRole";
 import SelectProperty from "./Component/SelectProperty";
-// import { StaffProvider } from "./Component/StaffContext";
 
 function App() {
   return (
-    
-    <div >
 
-      <Router>
-        <Routes>
-          <Route path="/" element={<ManageStaff />} />
-          <Route path="/add-staff" element={<AddStaff />} />
-          <Route path="/select-role" element={<SelectRole />} />
-          <Route path="/select-property" element={<SelectProperty />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ManageStaff />} />
+        <Route path="/add-staff" element={<AddStaff />} />
+        <Route path="/select-role" element={<SelectRole />} />
+        <Route path="/select-property" element={<SelectProperty />} />
+      </Routes>
+    </Router>
 
-    </div>
-  
   );
 }
 
