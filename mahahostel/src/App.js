42e97ef72373c3d46950d565a099e688dfc5506b
KarventhanFD <karventhankumar@gmail.com>
2025-03-03 11:35:17 +0530

    App file

--- mahahostel/src/App.js
+++ mahahostel/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import BedInventory from "./Common/DynamicComponents/BedInventory";
+import Test from "./Common/DynamicComponents/Test";
+import AddTenant from "./Common/DynamicComponents/AddTenant";
+import ScreenTenTenant from "./Common/DynamicComponents/ScreenTenTenant";
+import SuccessMsg from "./Common/DynamicComponents/SuccessMsg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import EditInventory from "./Common/DynamicComponents/EditInventory";
+import RoomSelection from "./Common/DynamicComponents/RoomSelection";
+import HostelBedSelection from "./Common/DynamicComponents/HostelBedSelection";
+import Inventory from "./Common/DynamicComponents/Inventory"
+import AddRoomType from "./Common/DynamicComponents/AddRoomType"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" style={{ fontFamily: "Montserrat" }}>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Inventory />} />
+        <Route path="/bedInventory" element={<BedInventory />} />
+        <Route path="/test" element= {<Test />}/>
+        <Route path="/addtenant" element= {<AddTenant />}/>
+        <Route path="/addtenant2" element= {<ScreenTenTenant />}/>
+        <Route path="/success" element= {<SuccessMsg />}/>
+        <Route path="/edit" element= {<EditInventory />}/>
+        <Route path="/roomselect" element= {<RoomSelection />}/>
+        <Route path="/bedselect" element= {<HostelBedSelection />}/>
+        <Route path="/addroom" element= {<AddRoomType />}/>
+      </Routes>
+    </Router>  
+    
+    
+    </div>  
   );
 }
 
