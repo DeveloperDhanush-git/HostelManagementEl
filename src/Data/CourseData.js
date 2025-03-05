9bdcae57e5ef2f8c29574c9140c1df3af574a570
Sneha Rajan <155979864+Sneha603@users.noreply.github.com>
2025-03-05 11:59:39 +0530

    Created CourseData.js

---
+++ src/Data/CourseData.js
@@ -0,0 +1,8 @@
+export const courses = [
+    { id: 1, title: "React for Beginners", rating: "4.5 & up", language: "English", category: "Technology" },
+    { id: 2, title: "Mastering JavaScript", rating: "4.0 & up", language: "English", category: "Technology" },
+    { id: 3, title: "Business Growth Strategies", rating: "3.5 & up", language: "Français", category: "Business" },
+    { id: 4, title: "Digital Marketing Basics", rating: "4.5 & up", language: "Español", category: "Business" },
+    { id: 5, title: "Graphic Design Fundamentals", rating: "4.0 & up", language: "English", category: "Art & Design" },
+  ];
+  
