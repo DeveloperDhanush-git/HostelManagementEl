ded7f262d8830388fb5e629f335f6c67294670c6
Sneha Rajan <155979864+Sneha603@users.noreply.github.com>
2025-03-05 12:00:46 +0530

    Add files via upload

---
+++ src/Data/filterOptions.js
@@ -0,0 +1,6 @@
+export const filterOptions = {
+    rating: ["4.5 & up", "4.0 & up", "3.5 & up", "3.0 & up"],
+    language: ["English", "Español", "Français", "Deutsch", "हिन्दी", "Português"],
+    category: ["Technology", "Business", "Art & Design"],
+  };
+  
